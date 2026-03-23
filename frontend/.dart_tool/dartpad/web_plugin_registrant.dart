40b5267e8edc2267f37b811e0b9b7efef9ccab71
alaaaa-ae <elbarkoukialaez@gmail.com>
2026-03-24 00:22:44 +0100

    Mobile responsiveness

--- frontend/.dart_tool/dartpad/web_plugin_registrant.dart
+++ frontend/.dart_tool/dartpad/web_plugin_registrant.dart
@@ -11,6 +11,7 @@ import 'package:file_picker/_internal/file_picker_web.dart';
 import 'package:geolocator_web/geolocator_web.dart';
 import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:share_plus/src/share_plus_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
@@ -22,6 +23,7 @@ void registerPlugins([final Registrar? pluginRegistrar]) {
   GeolocatorPlugin.registerWith(registrar);
   GoogleSignInPlugin.registerWith(registrar);
   ImagePickerPlugin.registerWith(registrar);
+  SharePlusWebPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
